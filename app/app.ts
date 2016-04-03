661153956e8f472f66a72d989627bbb1256adf91
Matthew Green <barmy_matt@hotmail.com>
2016-04-04 00:15:02 +0100

    cleanup

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
-import {Page} from "./decorators/page";
 import {App} from "./decorators/app";
-
 //pages 
 import { StartPage } from "./pages/startPage/startPage";
 import { RegionsPage } from "./pages/regionsPage/regionsPage";
@@ -9,15 +7,6 @@ import { RegionPage } from "./pages/regionPage/regionPage";
 import { RegionCompetitionPage } from "./pages/regionCompetitionPage/regionCompetitionPage";
 //providers 
 import {Logger} from "./providers/logger";
-
-//nx- control directives 
-//some reason you are all not appearing on each page... 
-import {NxNav} from "./controls/nav/nav";
-import {NxList} from "./controls/list/list";
-import {NxListItem} from "./controls/list/list-item";
-import {NxHeader} from "./controls/list/header";
-import {IonIcon} from "./controls/icons/ion-icon";
-
 import {RouteConfig} from "angular2/router";
 
 //app decorator - save some code writing. Wrapper around @Component
