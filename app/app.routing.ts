4aaeb70484f8475cc8f43331c7aac958c97d319b
Matthew Green <barmy_matt@hotmail.com>
2017-03-07 22:57:11 +0000

    fix routing at competition level. todo: restart complex listviews

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -9,16 +9,18 @@ export const appRoutes: Routes = [
     { path: "regions/:regionId/competition/:competitionId", component: Pages.CompetitionPage },
     { path: "regions/:regionId/competition/:competitionId/information", component: Pages.InformationPage },
     { path: "regions/:regionId/competition/:competitionId/startList", component: Pages.StartListPage },
+    { path: "regions/:regionId/competition/:competitionId/startList/:gradeId", component: Pages.StartListGradePage },
     { path: "regions/:regionId/competition/:competitionId/clubList", component: Pages.ClubListPage },
+    { path: "regions/:regionId/competition/:competitionId/clubList/:clubId", component: Pages.ClubPage },
     { path: "regions/:regionId/competition/:competitionId/gradeList", component: Pages.GradeListPage },
+    { path: "regions/:regionId/competition/:competitionId/gradelist/:gradeId", component: Pages.GradeCompetitorsPage },
+    
     //to-do - template & provider
     { path: "regions/:regionId/competition/:competitionId/findCompetitor", component: Pages.FindCompetitorPage },
     //to-do - template & provider
-    { path: "regions/:regionId/competition/:competitionId/stats", component: Pages.StatsPage },
+    { path: "regions/:regionId/competition/:competitionId/stats", component: Pages.StatsPage }
 
-    { path: "regions/:regionId/competition/:competitionId/startList/:gradeId/competitors", component: Pages.StartListGradePage },
     //to-do - template & provider
-    { path: "regions/:regionId/competition/:competitionId/grade/:gradeId/competitors", component: Pages.GradeCompetitorsPage },
     //to-do - template & provider
-    { path: "regions/:regionId/competition/:competitionId/club/:clubId/competitors", component: Pages.ClubPage },
+    
 ];
